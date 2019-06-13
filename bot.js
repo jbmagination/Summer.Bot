cc2c84d80bbaf7ad437b77c107e7115a7309ed6c
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-13 19:14:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,13 @@ client.on('ready', () => {
 client.on('message', message => {
   if (message.content.startsWith("r!")) {
     message.reply('the prefix is `0` for this server, not `r!`!');
-  };
-  else (if (message.content.startsWith("?")) {
+  }
+  else if (message.content.startsWith("?")) {
     message.reply('this server does not have Dyno! Please use RoboTop or Rythm!');
-  };
-  else (if (message.content.startsWith("=")) {
+  }
+  else if (message.content.startsWith("=")) {
     message.reply('this server does not have Ayana! Please use Rythm instead!');
-  };
+  }
 });    
 
 client.login(process.env.BOT_TOKEN);
