41a45d8953e463fb9fe3e3ca5f781588e0f1f5ae
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-14 16:09:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var version = '0.5'
-
+var version = '0.6'
+var catchphrase = 'Harro! s!help'
+var footer = version + ' | ' + catchphrase
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -12,26 +13,26 @@ message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
                       .addField('This server does not use `r!` for it\'s RoboTop prefix!','Please use `0` instead!')
                       .setTimestamp()
-                      .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+                      .setFooter(footer)
   )}
   else if (message.content.startsWith("?")) {
     message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
                       .addField('This server does not have Dyno!','Please use RoboTop or Rythm!')
                       .setTimestamp()
-                      .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+                      .setFooter(footer)
   )}
   else if (message.content.startsWith("=")) {
     message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
                       .addField('This server does not have Ayana!','Please use Rythm instead!')
                       .setTimestamp()
-                      .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+                      .setFooter(footer)
   )}
   else if (message.content.startsWith("<@588772311931420672>")) {
     message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
-                      .addField('Hi! I\'m a small helper for this server.','If you\'d to know more, DM <@381862688298631168> or use `s!help`')
+                      .addField('Harro! I\'m a special helper for this server, the Sunforest Ally!','If you\'d to know more, DM <@381862688298631168> or use `s!help` for a list of commands.')
                       .setTimestamp()
                       .setFooter(version)
  )}
@@ -42,19 +43,19 @@ message.channel.send(new Discord.RichEmbed()
                         .addField('s!help','Opens help/commands menu')
                         .addField('s!ping','Tests to see if bot is working')
                         .setTimestamp()
-   .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+   .setFooter(footer)
   )}
   else if (message.content === 's!ping') {
       message.channel.send(new Discord.RichEmbed()
     .setColor('#29752f')
     .addField('Pong!',new Date().getTime() - message.createdTimestamp + " ms")
     .setTimestamp()
-    .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+    .setFooter(footer)
   )} else message.channel.send(new Discord.RichEmbed()
                 .setColor('#29752f')
                 .addField('Sorry, I don\'t recognize that command!','Type `s!help` for a list of commands.')
                 .setTimestamp()
-                .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+                .setFooter(footer)
               )}
             });
 
