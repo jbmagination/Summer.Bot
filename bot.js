ad94912013a4196f656932acdd8c8872d0abe084
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-14 15:34:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var version = '0.4'
+var version = '0.5'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -35,22 +35,27 @@ message.channel.send(new Discord.RichEmbed()
                       .setTimestamp()
                       .setFooter(version)
  )}
-  else if (message.content === 's!help') {
-          message.channel.send(new Discord.RichEmbed()
-                      .setColor('#29752f')
-                      .addField('s!help','Opens help/commands menu')
-                      .addField('s!ping','Tests to see if bot is working')
-                      .setTimestamp()
- .setFooter(version + ' | Hi! I\'m a small helper for this server.')
-)}
-else if (message.content === 's!ping') {
-    message.channel.send(new Discord.RichEmbed()
-  .setColor('#29752f')
-  .addField('Pong!',new Date().getTime() - message.createdTimestamp + " ms")
-  .setTimestamp()
-  .setFooter(version + ' | Hi! I\'m a small helper for this server.')
-)
-  }
-});
+  else if (message.content.startsWith("s!")) {
+    if (message.content === 's!help') {
+            message.channel.send(new Discord.RichEmbed()
+                        .setColor('#29752f')
+                        .addField('s!help','Opens help/commands menu')
+                        .addField('s!ping','Tests to see if bot is working')
+                        .setTimestamp()
+   .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+  )}
+  else if (message.content === 's!ping') {
+      message.channel.send(new Discord.RichEmbed()
+    .setColor('#29752f')
+    .addField('Pong!',new Date().getTime() - message.createdTimestamp + " ms")
+    .setTimestamp()
+    .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+  )} else message.channel.send(new Discord.RichEmbed()
+                .setColor('#29752f')
+                .addField('Sorry, I don\'t recognize that command!','Type `s!help` for a list of commands.')
+                .setTimestamp()
+                .setFooter(version + ' | Hi! I\'m a small helper for this server.')
+              )}
+            });
 
 client.login(process.env.BOT_TOKEN);
