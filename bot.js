4b2246c4ea514b8827fba27010ed073d1d96f0ed
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-14 13:27:17 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var version = '0.3'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -7,20 +8,39 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content.startsWith("r!")) {
-    message.reply('the prefix is `0` for this server, not `r!`!');
+message.channel.send(new Discord.RichEmbed()
+                      .setColor('#29752f')
+                      .addField('This server does not have Dyno!','Please use RoboTop or Rythm!')
+                      .setTimestamp()
+                      .setFooter(version + ' | Hi! I\'m a small helper for this server. If you\'d to know more, DM <@381862688298631168>')
   }
   else if (message.content.startsWith("?")) {
-    message.reply('this server does not have Dyno! Please use RoboTop or Rythm!');
-  }
-  else if (message.content.startsWith("=")) {
-    message.reply('this server does not have Ayana! Please use Rythm instead!');
-  }
-  else if (message.content === '<@588772311931420672>') {
     message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
-                      .addField('Hi! I\'m a small helper for this server.','If you\'d to know more, DM <@381862688298631168>')
+                      .addField('This server does not have Dyno!','Please use RoboTop or Rythm!')
                       .setTimestamp()
-                      .setFooter('v0.2')
+                      .setFooter(version + ' | Hi! I\'m a small helper for this server. If you\'d to know more, DM <@381862688298631168>')
+  }
+  else if (message.content.startsWith("=")) {
+    message.channel.send(new Discord.RichEmbed()
+                      .setColor('#29752f')
+                      .addField('This server does not have Ayana!','Please use Rythm instead!')
+                      .setTimestamp()
+                      .setFooter(version + ' | Hi! I\'m a small helper for this server. If you\'d to know more, DM <@381862688298631168>')
+  }
+  else if (message.content.startsWith("<@588772311931420672>")) {
+    message.channel.send(new Discord.RichEmbed()
+                      .setColor('#29752f')
+                      .addField('Hi! I\'m a small helper for this server.','If you\'d to know more, DM <@381862688298631168> or use `s!help`')
+                      .setTimestamp()
+                      .setFooter(version)
+ }
+  else if (message.content === 's!help') {
+          message.channel.send(new Discord.RichEmbed()
+                      .setColor('#29752f')
+                      .addField('Drumroll...','I have no commands yet besides this one. That\'s right! NONE!')
+                      .setTimestamp()
+                      .setFooter(version)
 );
   }
 });    
