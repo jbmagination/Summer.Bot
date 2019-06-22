d21f4b1f4b71cfe7b829b421b04a6929f5589ecd
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-22 18:40:43 -0400

    We fixed it, let's hope it doesn't break again

--- bot.js
+++ bot.js
@@ -1,14 +1,47 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({disableEveryone: true});
 var version = '0.6'
 var catchphrase = 'Harro! s!help'
 var footer = version + ' | ' + catchphrase
-client.on('ready', () => {
+const fs = require("fs");
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+let messageArray = message.conent.split(" ")
+let cmd = messageArray[0];
+let args = messageArray.slice(1);
+let prefix = 's!'
+let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice((prefix.length)))
+// fs.readdir("./commands/", (err, files) => {
+//  if(err) console.log(err);
+//  let jsfile = files.filter(f => f.split(".").pop === "js")
+//  if(jsfile.length <= 0) {
+//    return console.log("Couldn't find commands");
+//  }
+
+//  jsfile.forEach((f, i) => {
+//    let pull = require(`./commands/${f}`);
+//    client.commands.set(pull.config.name, pull);
+//    pull.config.aliases.forEach(alias => {
+//      client.aliases.set(alias, pull.config.name)
+//    });
+//  });
+//});
+
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Harro! | s!help", {type: "STREAMING", url: "https://www.twitch.tv/summersunflower"});
 });
 
-client.on('message', message => {
-  if (message.content.startsWith("r!")) {
+client.on('message', async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") {
+    message.channel.send(new Discord.RichEmbed()
+    .setColor('#29752f')
+    .addField('You can\'t use this bot in DM\'s!','Head back to Sunforest Ally and use commands there!')
+    .setTimestamp()
+    .setFooter(footer)
+  }
+  else if (message.content.startsWith("r!")) {
 message.channel.send(new Discord.RichEmbed()
                       .setColor('#29752f')
                       .addField('This server does not use `r!` for it\'s RoboTop prefix!','Please use `0` instead!')
