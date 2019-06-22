4a25d50379bb523a94b3f3c416b903f1887f074a
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-06-22 19:19:28 -0400

    fix bot to client

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 let prefix = 's!'
-let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice((prefix.length))))
+let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice((prefix.length))))
 let guild = client.guilds.get('572499797991227403'), USER_ID = '551922470823657502';
 // fs.readdir("./commands/", (err, files) => {
 //  if(err) console.log(err);
