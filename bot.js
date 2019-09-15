e078b26c191eec1fe635e954a383f824d2afe10d
jbmagination <26027089+jbmagination@users.noreply.github.com>
2019-09-15 14:52:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var version = '0.7'
+var version = '0.8'
 var catchphrase = 'Harro! s!help'
 var footer = version + ' | ' + catchphrase
 client.on('ready', () => {
+  let statuses = [
+    `https://www.twitch.tv/princessnatsumihimawari`,
+    `https://www.twitch.tv/jbmagination`,
+    `a small ${client.users.size} users!`,
+    `just ${client.guilds.size} server!`,
+    `you type in this server`,
+    `a sunflower`,
+    `you read these status messages`,
+    `Discord`,
+    `@JBMagination#5512 code me`,
+    `you click to read this extended status message`,
+    `\${status_message}`,
+    ` `
+  ];
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`you find the elusive launch message. Congratulations!`, { type: 'WATCHING', status: 'online' });
+  setInterval(function() {
+    let status = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(status, { type: 'WATCHING', status: 'online' });
+  }, 20000);
+
 });
 
 client.on('message', message => {
@@ -36,7 +56,22 @@ message.channel.send(new Discord.RichEmbed()
                       .setTimestamp()
                       .setFooter(version)
  )}
-  else if (message.content.startsWith("s!")) {
+  else if (message.content === 's!harro')) {
+    message.channel.send(new Discord.RichEmbed()
+                      .setColor('#29752f')
+                      .addField('Harro! I\'m a special helper for this server, the Sunforest Ally!','If you\'d to know more, DM <@381862688298631168> or use `s!help` for a list of commands.')
+                      .setTimestamp()
+                      .setFooter(version)
+ )}
+  else if (message.content === 't!quote') {
+    let quote = [
+      `"When did we get so many children?!" -Summer`,
+      `"Ketchup is a fruit!" -Lala"`,
+      `"Ketchup is a smoothie!" -Lala`
+  ];
+    let quotes = quote[Math.floor(Math.random() * quote.length)];
+    message.channel.send(quotes);
+} else if (message.content.startsWith("s!")) {
     if (message.content === 's!help') {
             message.channel.send(new Discord.RichEmbed()
                         .setColor('#29752f')
